18bf4fe1e6c8043a008cbe0825e258115b9912ed
Leon Sommerer <leonsommerer03@gmail.com>
2021-01-26 15:40:04 +0100

    model kommentare

--- models/einkauf.js
+++ models/einkauf.js
@@ -1,20 +1,26 @@
-//Require Mongoose
 var mongoose = require('mongoose');
-
-//Define a schema
 var Schema = mongoose.Schema;
 
+// Schema Einkauf
 var Einkauf = new Schema({
+
+    // Erstelldatum
     date_begin: {
         type: Date,
         required: true
       },
+
+    // Kaufdatum
     date_end: {
         type: Date,
       },
+    
+    // Preis des Einkaufes
     sum: {
         type: Number,
       },
+    
+    // Items(Produkte) des Einkaufes
     items: {
       type: Array,
     }
